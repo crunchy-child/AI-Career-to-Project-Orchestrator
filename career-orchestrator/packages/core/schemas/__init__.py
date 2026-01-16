128852cf17ebdd752f16676b8e1bd83a100e1475
Minkyu Kim <kangaroo0305@gmail.com>
2026-01-16 01:00:43 -0800

    gg

--- career-orchestrator/packages/core/schemas/__init__.py
+++ career-orchestrator/packages/core/schemas/__init__.py
@@ -1,13 +1,13 @@
 from .keyword_base import BaseKeyword
-from .resume import ResumeProfile, ResumeEntry, ResumeBullet, ResumeEducation
-from .jd import JDProfile, JDKeyword, JDSection
-from .gap import GapSummary, KeywordEvidence, MatchScoreBreakdown
+from .resume import ResumeProfile, ResumeKeyword
+from .jd import JDProfile, JDKeyword
+from .gap import GapSummary, KeywordMatch
 from .project import ProjectOutput, ProjectPlan, ProjectIdea, ArchitectureSpec, DayPlan
 
 __all__ = [
     "BaseKeyword",
-    "ResumeProfile", "ResumeEntry", "ResumeBullet", "ResumeEducation",
-    "JDProfile", "JDKeyword", "JDSection",
-    "GapSummary", "KeywordEvidence", "MatchScoreBreakdown",
+    "ResumeProfile", "ResumeKeyword",
+    "JDProfile", "JDKeyword",
+    "GapSummary", "KeywordMatch",
     "ProjectOutput", "ProjectPlan", "ProjectIdea", "ArchitectureSpec", "DayPlan",
 ]
