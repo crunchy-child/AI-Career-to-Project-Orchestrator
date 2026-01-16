f514c633d3deb19536b94abb5fb2b92d4a7d17e5
Minkyu Kim <kangaroo0305@gmail.com>
2026-01-15 17:14:30 -0800

    Refactor keyword schemas

--- career-orchestrator/packages/core/schemas/__init__.py
+++ career-orchestrator/packages/core/schemas/__init__.py
@@ -1,9 +1,11 @@
+from .keyword_base import BaseKeyword
 from .resume import ResumeProfile, ResumeEntry, ResumeBullet, ResumeEducation
 from .jd import JDProfile, JDKeyword, JDSection
 from .gap import GapSummary, KeywordEvidence, MatchScoreBreakdown
 from .project import ProjectOutput, ProjectPlan, ProjectIdea, ArchitectureSpec, DayPlan
 
 __all__ = [
+    "BaseKeyword",
     "ResumeProfile", "ResumeEntry", "ResumeBullet", "ResumeEducation",
     "JDProfile", "JDKeyword", "JDSection",
     "GapSummary", "KeywordEvidence", "MatchScoreBreakdown",
